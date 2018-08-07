7bcbb4c568b48792e88cc6aafd53152be9c0b921
Joseph Davis <JNDavis1@dstsystems.com>
2018-08-07 14:00:43 -0500

    Add search bar

---
+++ src/containers/SearchBar.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(e) {
+    this.setState({ term: e.target.value });
+  }
+
+  onFormSubmit(e) {
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <form className="input-group" onSubmit={this.onFormSubmit}>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Get a five-day forecast in your favorite cities"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">
+            Submit
+          </button>
+        </span>
+      </form>
+    );
+  }
+}
